eaaa9513d97374dc7ee847146465e0efdb413cea
V6 <whyungee@naver.com>
2025-03-25 10:46:39 +0900

    매물등록 구현완료- 이미지 구현전 커밋

--- src/main/java/com/realestate/app/RealStateAppApplication.java
+++ src/main/java/com/realestate/app/RealStateAppApplication.java
@@ -1,5 +1,7 @@
 package com.realestate.app;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RealStateAppApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("src/main/resources")
+				.ignoreIfMissing()
+				.load();
+
+		// 환경변수 등록
+		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+
 		SpringApplication.run(RealStateAppApplication.class, args);
 	}
 
